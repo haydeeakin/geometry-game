7dde9dc63fe97fa656ff74ec00901fda8e4151c4
Brendan Stillwell <stillwellbrendan@gmail.com>
2020-01-10 15:44:11 -0700

    built and tested functions for translation rotation and reflection.  Contributed by Brendan, Danny and Mark

---
+++ transformations.test.js
@@ -0,0 +1,22 @@
+import {translation, rotation, reflection} from './transformations.js'
+
+test('translation', () => {
+    let testCoordinate = [6,3];
+    expect(translation(testCoordinate, -5, 'x')).toEqual([1,3]);
+    expect(translation(testCoordinate, -5, 'y')).toEqual([1,-2]);
+    expect(translation(testCoordinate, 2, 'y')).toEqual([1,0]);
+
+})
+test('rotation', () => {
+    let testCoordinate = [4,6];
+    expect(rotation(testCoordinate, 90, [0,0])).toEqual([-6,4]);
+    expect(rotation(testCoordinate, 90, [0,0])).toEqual([-4,-6]);
+    expect(rotation(testCoordinate, 90, [0,0])).toEqual([6,-4]);
+    expect(rotation(testCoordinate, 90, [0,0])).toEqual([4,6]);
+})
+test('reflection', () => {
+    let testCoordinate = [1,3];
+    expect(reflection(testCoordinate, [0,1,0])).toEqual([1,-3]);
+    expect(reflection(testCoordinate, [1,0,0])).toEqual([-1,-3]);
+    expect(reflection(testCoordinate, [1,0,-1])).toEqual([3,-3]);
+})
