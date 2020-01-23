c69bdfcbec9c8a41b747297616b5f0d472502a29
Brendan Stillwell <stillwellbrendan@gmail.com>
2020-01-23 10:17:45 -0700

    checking to see if queue file committed

--- geometryapplication/src/game/queue.js
+++ geometryapplication/src/game/queue.js
@@ -6,18 +6,82 @@ class Queue extends React.Component{
         super();
         this.state = {
             queue: [],
-            interface: 'translation'        //default new transformation
+            interface: '',
+            reflectionBox: true     //disable number box for reflection
+        }
+    }
+    interfaceUpdate = (event) => {
+        this.setState({
+            interface: event.target.value
+        })
+    }
+    reflectionBoxUpdate = (event) => {
+        if(event.target.value === "x" ||event.target.value === "y"){
+            this.setState({
+                reflectionBox: false
+            })
+        }else{
+            this.setState({
+                reflectionBox: true
+            })
+        }
+
+    }
+    newActionSelector(){
+        if(this.state.interface === "translation"){
+            return(
+                <div>
+                    <select>
+                        <option value="x">X</option>
+                        <option value="y">Y</option>
+                    </select>
+                    <input type="number" name="x/yValue" min="-20" max="20" defaultValue="0"></input>
+                    <button style={{height: 20, width: 50}}>Add</button>
+                </div>
+            )
+        }
+        if(this.state.interface === "rotation"){
+            return(
+                <div>
+                    <select>
+                        <option value="clockwise">Clockwise</option>
+                        <option value="counterClockwise">Counter Clockwise</option>
+                    </select>
+                    <select>
+                        <option value="90">90&deg;</option>
+                        <option value="180">180&deg;</option>
+                        <option value="270">270&deg;</option>
+                    </select>
+                    <button style={{height: 20, width: 50}}>Add</button>
+                </div>
+            )
+        }
+        if(this.state.interface === "reflection"){
+            return(
+                <div>
+                    <select onChange={this.reflectionBoxUpdate}>
+                        <option value="xAxis">X Axis</option>
+                        <option value="yAxis">Y Axis</option>
+                        <option value="x">X=</option>
+                        <option value="y">Y=</option>
+                    </select>
+                    <input type="number" name="x/yValue" min="-20" max="20" defaultValue="0" hidden={this.state.reflectionBox}></input>
+                    <button style={{height: 20, width: 50}}>Add</button>
+                </div>
+            )
         }
     }
     render(){
         return(
             <div id="actionQueue">
                 <h1>Action Queue</h1><br/>
-                <select>
+                <select onChange={this.interfaceUpdate}>
+                    <option>Select Transformation</option>
                     <option value="translation">Translation</option>
                     <option value="rotation">Rotation</option>
                     <option value="reflection">Reflection</option>
                 </select>
+                {this.newActionSelector()}
             </div>
         )
     }
