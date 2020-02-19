99813d9839471d0f1b78b109b02e11030f017906
haydeeakin <peraltavazquezhaydee@gmail.com>
2020-02-19 11:20:01 -0700

    login

---
+++ geometryapplication/src/routes/Login.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { gql, graphql } from 'react-apollo';
+
+class Login extends React.Component {
+    state = {
+        email: '',
+        password: '',
+    }
+    
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    onSubmit = async () => {
+        const response = await this.props.mutate({
+            variables: this.state,
+        });
+        console.log(response);
+    }
+
+    render() {
+        return (
+            <div>
+                <Input
+                name='email'
+                placeholder='Email'
+                onChange={e => this.onChange(e)}
+                value={this.state.email} />
+                <Input
+                name= 'password'
+                placeholder= 'Password'
+                type='password'
+                onChange={e => this.onChange(e)}
+                value={this.state.password} />
+                <br />
+                <Button onClick={() => this.onSubmit()} type='primary'>Login</Button>
+            </div>
+        );
+    }
+}
