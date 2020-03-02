096f281ebd4093eec76ead61305fff3985ea2599
dannygit2019 <danny.tran.dba@gmail.com>
2020-03-02 10:22:39 -0700

    font color changed

--- geometryapplication/src/game/listItem.js
+++ geometryapplication/src/game/listItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ListItem(props){
     return(
-        <div style={{color: "white", fontSize:23, textAlign:"left", paddingLeft: 10}}>
+        <div style={{color: "black", fontSize:21, textAlign:"left", paddingLeft: 10}}>
             {props.typeTransform}: {props.type} {props.value} {props.location}
         </div>
     )
